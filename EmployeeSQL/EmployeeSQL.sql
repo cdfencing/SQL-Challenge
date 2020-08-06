0ca3408324e2826ce63f992bb932b5ecf7117786
cdfencing <65666202+cdfencing@users.noreply.github.com>
2020-08-06 18:04:42 -0500

    table edits

--- EmployeeSQL/EmployeeSQL.sql
+++ EmployeeSQL/EmployeeSQL.sql
@@ -1,44 +1,42 @@
 --Table creation
-
---not null not working
 CREATE TABLE Departments (
-	dept_no VARCHAR PRIMARY KEY,
-	dept_name VARCHAR
+	dept_no VARCHAR(50) NOT NULL PRIMARY KEY,
+	dept_name VARCHAR(50) NOT NULL
 );
 
 SELECT * FROM Departments
 
 CREATE TABLE Titles (
-	title_id VARCHAR PRIMARY KEY,
-	title VARCHAR
+	title_id VARCHAR(50) NOT NULL PRIMARY KEY,
+	title VARCHAR(50) NOT NULL
 );
 
 SELECT * FROM Titles
 
 CREATE TABLE Employees (
-	emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR,
-	birth_date DATE,
-	first_name VARCHAR,
-	last_name VARCHAR,
-	sex VARCHAR(1),
-	hire_date DATE,
+	emp_no INT NOT NULL PRIMARY KEY,
+	emp_title_id VARCHAR(50) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL,
 	FOREIGN KEY(emp_title_id) REFERENCES Titles(title_id)
 );
 
 SELECT * FROM Employees
 
 CREATE TABLE Salaries (
-	emp_no INT PRIMARY KEY,
-	salary INT,
+	emp_no INT NOT NULL PRIMARY KEY,
+	salary INT NOT NULL,
 	FOREIGN KEY(emp_no) REFERENCES Employees(emp_no)
 );
 
 SELECT * FROM Salaries
 
 CREATE TABLE Dept_Emp (
-	emp_no INT,
-	dept_no VARCHAR,
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(50),
 	PRIMARY KEY(emp_no, dept_no),
 	FOREIGN KEY(emp_no) REFERENCES Employees(emp_no),
 	FOREIGN KEY(dept_no) REFERENCES Departments(dept_no)
@@ -47,8 +45,8 @@ CREATE TABLE Dept_Emp (
 SELECT * FROM Dept_Emp
 
 CREATE TABLE Dept_Manager (
-	dept_no VARCHAR,
-	emp_no INT,
+	dept_no VARCHAR(50) NOT NULL,
+	emp_no INT NOT NULL,
 	PRIMARY KEY(dept_no, emp_no),
 	FOREIGN KEY(emp_no) REFERENCES Employees(emp_no),
 	FOREIGN KEY(dept_no) REFERENCES Departments(dept_no)
